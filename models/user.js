0e0be181adf4b76df7bd7dc8daff28d7c43ba3c6
dongjinc <whsso1101@ajou.ac.kr>
2019-04-18 19:53:34 +0900

    add travis

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: {type: String, required: true},
   StudentId: { type: String, default:"9999" }, //prof == "9999"
-  ID: {type: String, required: true},
+  email: {type: String, required: true},
   password: {type: String, required: true},
   date: {type: Date, default: Date.now},
   classList : [
