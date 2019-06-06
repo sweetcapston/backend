c1eaa172f51b63b7c850472b3c649ff1ff50c9ac
dongjinc <whsso1101@ajou.ac.kr>
2019-06-06 16:10:56 +0900

    질문스키마수정

--- models/question.js
+++ models/question.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const QuestionSchema = new mongoose.Schema({
   classCode: { type: String, require: true },
+  QesID: { type: Number },
   userID: { type: String, required: true },
   studentID: { type: String, required: true },
   userName: { type: String, required: true },
@@ -11,6 +13,8 @@ const QuestionSchema = new mongoose.Schema({
   likeList: [{type: String}]
 });
 
+
+QuestionSchema.plugin(AutoIncrement, {id: 'QesID_seq',inc_field: 'QesID'});
 const Question = mongoose.model('Question', QuestionSchema);
 
 module.exports = Question;
